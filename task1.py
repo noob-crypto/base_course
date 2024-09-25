bd22eb5d77202ee0516ddc0f260a6171682e378c
noob-crypto <lexa30122008@gmail.com>
2024-09-25 16:03:56 +0000

    1дз1

---
+++ task1.py
@@ -0,0 +1,7 @@
+print ('Hello, World!')
+
+print ('Маша + Петя = Любовь')
+
+print ('х = 3 + 4')
+
+print(f'x = {3 + 4}')
---
+++ task2.py
@@ -0,0 +1,9 @@
+type('hello world')
+print (type('hello world'))
+
+type(3 + 4)
+print (type(3 + 4))
+
+type(3 / 4)
+print(type(3 / 4))
+
